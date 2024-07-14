8ca497490c2c5b6fb2e0759a2a1aabd129bb6400
Macdonald Manana <macddeemanana@gmail.com>
2024-07-14 16:11:51 +0200

    new migrations

--- psba_app/models.py
+++ psba_app/models.py
@@ -16,7 +16,7 @@ class Motorist(models.Model):
 class ParkingSpace(models.Model):
     type = models.CharField(max_length=10)
     address = models.CharField(max_length = 255)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    price = models.DecimalField(max_digits=100, decimal_places=2, null=True)
     latitude = models.DecimalField(max_digits=10, decimal_places=6)
     longitude = models.DecimalField(max_digits=10, decimal_places=6)
     prepaid = models.BooleanField()
